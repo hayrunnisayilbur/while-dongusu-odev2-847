2c50bee24d259473ace4e609d1b6fc6e30202744
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-21 14:06:08 +0300

    Add files via upload

---
+++ while döngüsü ödev2-847/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_döngüsü_ödev2_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbSayilar1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sayi = 1;
+            do
+            {
+                lbSayilar1.Items.Add(sayi);
+                sayi++;
+            } while (sayi <= 10);
+        }
+    }
+}
